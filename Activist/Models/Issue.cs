53be9849bfd93c99fb24fb2a87fff4de413aa26a
tcpugh05 <tcpugh05@gmail.com>
2014-05-25 12:35:55 -0400

    Got it to work in production when deploying.

---
+++ Activist/Models/Issue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Activist.Models
+{
+    public class Issue {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+    }
+    public class IssueDBContext: DbContext
+    {
+        public DbSet<Issue> Issues{get;set;}
+    }
+}
